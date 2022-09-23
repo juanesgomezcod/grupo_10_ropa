2f2aeb238debe3cf204e4bcf3ffbb0e7c48ebca5
juanesgomezcod <juanesgomez@gmail.com>
2022-09-23 18:19:15 -0500

    formulario login

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "src/views/login.html"));
+});
+
 app.listen(3000,() => console.log("Servidor corriendo"))
 
 // app.get("/register", (req,res) => {
