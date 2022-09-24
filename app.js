3edd384e0bd7c2966412305af8c75edc87ea312c
JuliethPaolaOlarte <julietholarte16@gmail.com>
2022-09-23 23:54:57 -0500

    Carrito de compras

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
+app.listen(3000,() => console.log("Servidor corriendo"))
+
 app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, "./src/views/index.html"))
 });
 
-app.listen(3000,() => console.log("Servidor corriendo"))
-
 // app.get("/register", (req,res) => {
 //     res.sendFile(path.resolve(__dirname, "./src/views/register.html"));
 // });
+
+app.get("/productCart", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./src/views/productCart.html"))
+});
