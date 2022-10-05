59b7e02ba80890c27181845141c22609acd52f7b
juanesgomezcod <juanesgomez@gmail.com>
2022-10-05 16:33:24 -0500

    cambio temporal

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require("express");
 var path = require ("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public")));
-app.set('views',(path.join(__dirname ,"views"))); 
+app.use(express.static(path.join(__dirname, "/Public")));
+app.set('views',(path.join(__dirname ,"./src/views"))); 
 app.set('view engine', 'ejs');
 const mainRoutes = require('./src/routes/mainRoutes');
 
-// app.get("/", function(req, res){
-//     res.sendFile(path.resolve(__dirname, "./src/views/index.ejs"))
-// });
+
 
 app.use('/', mainRoutes);
 
