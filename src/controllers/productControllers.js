f3f52fd8b18360e42c2ce70ac37eb999889a8ca2
juanesgomezcod <juanesgomez@gmail.com>
2022-10-05 21:27:12 -0500

    rutas productDetail

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,10 +1,51 @@
+let productos = [
+    {
+        id:1,
+        nombreProducto:'Camiseta Azul',
+        descripcion: 'Camiseta Azul con estampado amarillo',
+        categoria: 'Remeras',
+        img: 'producto1.jpg',
+        tamaño: 'L',
+        precio: '39500'
+    },
+    {
+        id:2,
+        nombreProducto:'Tote Bag con Negro',
+        descripcion: 'Tote Bag Beige con estampado en negro',
+        categoria: 'Tote Bag',
+        img: 'producto2.jpg',
+        tamaño: 'S',
+        precio: '39500'
+    },
+    {
+        id:3,
+        nombreProducto:'Tote Bag con Cafe',
+        descripcion: 'Tote Bag Beige con estampado en cafe',
+        categoria: 'Tote Bag',
+        img: 'producto3.jpg',
+        tamaño: 'S',
+        precio: '39500'
+    },
+    {
+        id:4,
+        nombreProducto:'Jean Cruces',
+        descripcion: 'Jean desgastado con estampados de cruces negras',
+        categoria: 'Jeans',
+        img: 'producto4.jpg',
+        tamaño: 'XL',
+        precio: '39500'
+    }
+]
+
 const controller = {
     store: (req, res) => {
         res.render("store");
     }, 
 
     productDetail: (req, res) => {
-        res.render("productDetail");
+        let productId = req.params.id;
+        let product = productos.find(product => product.id == productId);
+        res.render("productDetail", {product});
     }, 
 
     productCart: (req, res) => {
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,7 +5,9 @@ const controller = require('../controllers/productControllers');
 
 router.get('/store', controller.store);
 router.get('/productDetail', controller.productDetail);
+router.get('/productDetail/:id', controller.productDetail);
 router.get('/newProduct', controller.newProduct);
 router.get('/productCart', controller.productCart);
+
 
 module.exports = router;
