96091e15e155653718bbfe8030f0d691fe3af2a6
juanesgomezcod <juanesgomez@gmail.com>
2022-12-15 16:16:04 -0500

    modelos

--- db/swipdb.sql
+++ db/swipdb.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE productos_db;
+CREATE DATABASE swip_db;
 
-USE productos_db;
+USE swip_db;
 
 --
--- Estructura de tabla para la tabla `products`
+-- Estructura de tabla: `products`
 --
 
 CREATE TABLE Productos  (
@@ -32,7 +32,7 @@ ALTER TABLE Productos  ADD  FOREIGN KEY (id_talla) REFERENCES tallas(id)
 
 -- --------------------------------------------------------
 --
--- Estructura de tabla para la tabla `users`
+-- Estructura de: `users`
 --
 
 CREATE TABLE Usuarios (
@@ -47,3 +47,69 @@ CREATE TABLE Usuarios (
 )
 
 
+----------------------------------------------------------
+
+
+--
+-- Script de población de datos 
+--
+
+
+-- categorias
+
+insert into categorias  (id, categoria) values (1, 'hoodies');
+insert into categorias  (id, categoria) values (2, 'jeans'); 
+insert into categorias  (id, categoria) values (3, 'remeras');
+insert into categorias  (id, categoria) values (4, 'camperas');
+insert into categorias  (id, categoria) values (5, 'tote bag');
+
+-- tallas
+
+insert into tallas (id, talla) values (1, 'XS');
+insert into tallas (id, talla) values (2, 'S');
+insert into tallas (id, talla) values (3, 'M');
+insert into tallas (id, talla) values (4, 'L');
+insert into tallas (id, talla) values (5, 'XL');
+insert into tallas (id, talla) values (6, 'unica');
+
+-- productos
+
+insert into productos (id, nombre, descripcion, precio, id_talla, id_categoria) values (1, 'hoodie AstroWorld', 'hoodie estampado', '95000' , '3', '1');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (2, 'jean glam', 'jean estampando pintando', '90000', '3', '2');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (3, 'remera aesthetic', 'remera con estampado aesthetic', '90000', '1', '3');
+insert into productos (id, nombre, descripcion, precio, id_talla, id_categoria) values (4, 'campera glow', 'campera tornazol', '150000', '2', '4');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (5, 'tote bag butterfly', 'tote bag con estampado de mariposa', '60000', '6', '5');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (6, 'hoodie over-size', 'hoodie oversize, estampado', '100000', '4', '1');
+insert into productos  (id, nombre, descripcion,precio, id_talla, id_categoria) values (7, 'jean hearts', 'jeans con estampado de corazones', '100000', '3', '2');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (8, 'remera shine', 'remera tornazol estampada', '40000', '1', '3');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (9, 'campera glow', 'campera gris brillante', '100000', '4', '4');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (10, 'tote bag stars', 'tote bag con estampado de estrellas', '100000', '6', '5');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (11, 'hoodie X', 'hoodie estampado X', '100000',  '3', '1');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (12, 'jean lux', 'jean pintado lux', '100000',  '2', '2');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (13, 'remera Brooklyn', 'remera estampado Brooklyn', '100000', '1', '3');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (14, 'campera NYC', 'campera estampado NYC', '100000', '2', '4');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (15, 'tote bag bussines', 'tote bag estampada', '100000', '6', '5');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (16, 'hoodie moscow mule', 'hoodie estampado playa', '130000', '5', '1');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (17, 'jean artic', 'jean pintado artic', '90000', '2', '2');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (18, 'remera black', 'remera básica', '50000', '3', '3');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (19, 'campera green-reed', 'campera divida en dos colores', '160000', '3', '4');
+insert into productos  (id, nombre, descripcion, precio, id_talla, id_categoria) values (20, 'tote bag plants', 'tote bag estampado de plantas',  '65000', '6', '5');
+
+-- usuarios
+
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (1, 'Clayson', 'Nutton', 'cnutton0@elpais.com', 'Z3nLHNVV', 0, 'https://robohash.org/debitisaperiamconsequatur.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (2, 'Evaleen', 'Ackery', 'eackery1@123-reg.co.uk', 'QoceQ2m1', 1, 'https://robohash.org/nostrumquiqui.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (3, 'Aguistin', 'Fielden', 'afielden2@exblog.jp', 'vRYJSg65xJ', 0, 'https://robohash.org/quianamdicta.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (4, 'Neall', 'Helliwell', 'nhelliwell3@harvard.edu', 'EqhtqlsOBP', 0, 'https://robohash.org/siteaquea.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (5, 'Kaylil', 'Sheals', 'ksheals4@ftc.gov', 'T5afrtxFC', 0, 'https://robohash.org/suntenimqui.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (6, 'Ely', 'Dannohl', 'edannohl5@weather.com', '33RSeNv', 0, 'https://robohash.org/natusipsamaiores.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (7, 'Raynell', 'Kinkade', 'rkinkade6@marketwatch.com', 'gWts2dH', 0, 'https://robohash.org/perferendisdelectusporro.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (8, 'Lyn', 'Haslen', 'lhaslen7@purevolume.com', 'KgxbOUT8oEz', 0, 'https://robohash.org/utvoluptasmaxime.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (9, 'Josy', 'Mussington', 'jmussington8@google.fr', 'APHhGko', 1, 'https://robohash.org/laudantiumnihilquod.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (10, 'Prudence', 'Klemencic', 'pklemencic9@t.co', 'RhZBPDSVP', 1, 'https://robohash.org/sedaliasest.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (11, 'Sansone', 'Iskowicz', 'siskowicza@goo.gl', 'dgg5O6ysa7v', 1, 'https://robohash.org/laborumsaepesit.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (12, 'Shara', 'Lothlorien', 'slothlorienb@sfgate.com', '9q3zjX6c', 1, 'https://robohash.org/omnisexpeditaexplicabo.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (13, 'Creight', 'Ganders', 'cgandersc@clickbank.net', 'ecBOQwJyI', 1, 'https://robohash.org/quosadipiscinam.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (14, 'Odella', 'Stockell', 'ostockelld@nih.gov', 'gtiz259Fu', 0, 'https://robohash.org/quiaeosreprehenderit.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (15, 'Shea', 'Barnby', 'sbarnbye@abc.net.au', 'QIhzLy3i9t', 1, 'https://robohash.org/porroeligendiut.png?size=50x50&set=set1');
+insert into usuarios (id, nombres, apellidos, email, contraseña, admin, avatar) values (16, 'Leann', 'Hammerman', 'lhammermanf@nifty.com', '10VLd3K', 0, 'https://robohash.org/perferendisminimaet.png?size=50x50&set=set1');
