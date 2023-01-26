02614cc1bdf3ff987ec40dda1e555f768d30bc00
JuliethPaolaOlarte <julietholarte16@gmail.com>
2023-01-26 03:31:26 -0500

    mini arreglo

--- src/middlewares/ValidationLogged.js
+++ src/middlewares/ValidationLogged.js
@@ -2,8 +2,9 @@ const {check} = require("express-validator");
 const db = require("../database/models");
 
 const validationLogged = [
-  check("email").notEmpty().withMessage("Ingresa tú email").bail().isEmail().withMessage("Por favor, ingresa un email válido").equals(db.User.email).withMessage('las contraseñas no coinciden'),
-  check("clave").notEmpty().withMessage("Debes ingresar una contraseña").bail().equals(db.User.clave).withMessage('Datos inválidos')
+  check("email").notEmpty().withMessage("Ingresa tú email").bail().isEmail().withMessage("Por favor, ingresa un email válido"),
+  check("clave").notEmpty().withMessage("Debes ingresar una contraseña").bail()
 ];
 
 module.exports = validationLogged;
+
